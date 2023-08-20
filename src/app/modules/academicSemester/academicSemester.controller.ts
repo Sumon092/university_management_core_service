4ca0d09c32d2d4fcfd743a9b653a641088a7c180
Sumon <m.sumon@outlook.com>
2023-08-20 20:08:35 +0600

    code modified with catchAsync

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,19 +1,19 @@
+import { AcademicSemester } from '@prisma/client';
 import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
 import { academicSemesterService } from './academicSemester.services';
 
-const addSemester = async (req: Request, res: Response) => {
-  try {
-    const result = await academicSemesterService.addSemester(req.body);
-    res.json({
-      status: 200,
-      success: true,
-      message: 'Semester added successfully',
-      data: result,
-    });
-  } catch (error) {
-    res.send(error);
-  }
-};
+const addSemester = catchAsync(async (req: Request, res: Response) => {
+  const result = await academicSemesterService.addSemester(req.body);
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester added successfully',
+    data: result,
+  });
+});
 
 export const semesterController = {
   addSemester,
