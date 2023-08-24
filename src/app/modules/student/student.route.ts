cb667976d1ec364c13e1f1c944fcb40edcb03969
Sumon <m.sumon@outlook.com>
2023-08-24 17:54:29 +0600

    student validation added

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { studentController } from './student.controller';
+import { StudentValidation } from './student.validation';
 
 const router = express.Router();
 
-router.post('/create-student', studentController.createStudent);
+router.post(
+  '/create-student',
+  validateRequest(StudentValidation.create),
+  studentController.createStudent
+);
 
 export const studentRoutes = router;
