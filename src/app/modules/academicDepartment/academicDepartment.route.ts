fedbbdfc2c68e7e971769bfb53e0718568b8c5a3
Sumon <m.sumon@outlook.com>
2023-08-24 17:13:25 +0600

    department validation added

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { departmentController } from './academicDepartment.controller';
+import { AcademicDepartmentValidation } from './academicDepartment.validation';
 
 const router = express.Router();
 
-router.post('/create-department', departmentController.createDepartment);
+router.post(
+  '/create-department',
+  validateRequest(AcademicDepartmentValidation.create),
+  departmentController.createDepartment
+);
 router.get('/', departmentController.getDepartments);
 router.get('/:id', departmentController.getDepartment);
 
