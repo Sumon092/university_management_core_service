42d7047aa9f1a8a51aed01b32a619fc2892d9c58
Sumon <m.sumon@outlook.com>
2023-08-24 17:16:08 +0600

    academic faculty validation added

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { academicFacultyController } from './academicFaculty.controller';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
 
 const router = express.Router();
 
 router.get('/get-faculty/:id', academicFacultyController.getFaculty);
 router.get('/', academicFacultyController.getFaculties);
-router.post('/create-faculty', academicFacultyController.createFaculty);
+router.post(
+  '/create-faculty',
+  validateRequest(AcademicFacultyValidation.create),
+  academicFacultyController.createFaculty
+);
 
 export const academicFacultyRoutes = router;
