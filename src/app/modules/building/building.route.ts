b269023080047c5a5b445fdb2f85b2e57087c874
Sumon <m.sumon@outlook.com>
2023-08-30 13:00:58 +0600

    roles based access added to building and rooms

--- src/app/modules/building/building.route.ts
+++ src/app/modules/building/building.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
 import { BuildingValidationSchema } from './building.validation';
@@ -12,13 +14,36 @@ router.patch(
 router.post(
   '/create-building',
   validateRequest(BuildingValidationSchema.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   BuildingController.createBuilding
 );
 
-router.get('/', BuildingController.getAllBuilding);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  BuildingController.getAllBuilding
+);
 
-router.get('/:id', BuildingController.getSingleBuilding);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  BuildingController.getSingleBuilding
+);
 
-router.delete('/:id', BuildingController.deleteBuilding);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  BuildingController.deleteBuilding
+);
 
 export const BuildingRoutes = router;
--- src/app/modules/rooms/room.route.ts
+++ src/app/modules/rooms/room.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { RoomController } from './room.controller';
 import { RoomValidationSchema } from './room.validation';
@@ -6,19 +8,43 @@ const router = express.Router();
 
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(RoomValidationSchema.update),
   RoomController.updateRoom
 );
 router.post(
   '/create-room',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(RoomValidationSchema.create),
   RoomController.createRoom
 );
 
-router.get('/', RoomController.getAllRoom);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  RoomController.getAllRoom
+);
 
-router.get('/:id', RoomController.getSingleRoom);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  RoomController.getSingleRoom
+);
 
-router.delete('/:id', RoomController.deleteRoom);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  RoomController.deleteRoom
+);
 
 export const RoomRoutes = router;
