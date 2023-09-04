dedab3aa272bd81c3d07ada4551515a080ae6329
Sumon <m.sumon@outlook.com>
2023-09-04 15:24:13 +0600

    validation error

--- src/errors/handleValidationError.ts
+++ src/errors/handleValidationError.ts
@@ -1,42 +1,17 @@
-// import { IGenericErrorResponse } from '../interfaces/common';
-// import { IGenericErrorMessage } from '../interfaces/error';
-
-// const handleValidationError = (
-//   error: mongoose.Error.ValidationError
-// ): IGenericErrorResponse => {
-//   const errors: IGenericErrorMessage[] = Object.values(error.errors).map(
-//     (el: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
-//       return {
-//         path: el?.path,
-//         message: el?.message,
-//       };
-//     }
-//   );
-//   const statusCode = 400;
-//   return {
-//     statusCode,
-//     message: 'Validation Error',
-//     errorMessages: errors,
-//   };
-// };
-
-// export default handleValidationError;
-
-import { ValidationError } from 'sequelize'; // Import the appropriate validation error type for your Sequelize setup
+import { Prisma } from '@prisma/client';
 import { IGenericErrorResponse } from '../interfaces/common';
-import { IGenericErrorMessage } from '../interfaces/error';
 
 const handleValidationError = (
-  error: ValidationError
+  error: Prisma.PrismaClientValidationError
 ): IGenericErrorResponse => {
-  const errors: IGenericErrorMessage[] = error.errors.map(el => {
-    return {
-      path: el.path as string, // Column or field name that caused the validation error
-      message: el.message, // Error message describing the validation failure
-    };
-  });
+  const errors = [
+    {
+      path: '',
+      message: error.message,
+    },
+  ];
 
-  const statusCode = 400; // You can adjust the status code as needed
+  const statusCode = 400;
   return {
     statusCode,
     message: 'Validation Error',
